74b8d6d641fb4555e490fd08447c786ed31d4478
Mohammad Abdul Sahil || Python Programmer <127765312+abdulsaheel@users.noreply.github.com>
2024-09-19 12:24:36 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,12 +2,20 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 // Create a proxy server
-const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({
+  secure: false, // Ignore certificate errors
+});
 
-// Create a server that listens on port 8080
+// Create an HTTP server that listens on port 3000
 const server = http.createServer((req, res) => {
-  // Forward the request to localhost:8443
-  proxy.web(req, res, { target: 'https://162.55.81.185:8443' });
+  // Forward the request to the target server with HTTPS
+  proxy.web(req, res, { target: 'https://162.55.81.185:8443' }, (err) => {
+    if (err) {
+      console.error('Proxy error:', err);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Proxy error');
+    }
+  });
 });
 
 // Listen on port 3000
